bb0386a0e473db934b1d7d65062b46e532bd4ba8
diego <diego@inovarfacil.(none)>
2013-06-04 21:18:23 -0300

    Aplicação

---
+++ ofsystem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ofsystem::Application.config.secret_token = 'f041fcf42d3c3aa700ae0bafab77f6e82c941588b520e960dbb79ab05f5ed41d90078acf38341ee175b951d31ed0dd726333bb62b7c737cef1a2b71a232e0bbf'
